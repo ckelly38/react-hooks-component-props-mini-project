a4fa09a7c17cabf94909456d0552ea14ff787f40
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-18 22:46:04 -0600

    it sets up the bonus min read feature on the article display

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,40 @@ import React from "react";
 
 function Article({title, preview, minutes, date = "January 1, 1970"})
 {
+    let minutesisnotvalid = false;
+    if (minutes === undefined || minutes === null || isNaN(minutes))
+    {
+        minutesisnotvalid = true;
+    }
+    else
+    {
+        if (minutes < 0) minutesisnotvalid = true;
+        //else;//do nothing
+    }
+
+    const mycupimgchar = "☕️";//for less than 30 minutes add one every 5 minutes
+    const mybrkfstimgchar = "🍱";//for 30 minutes or more add one every 10 minutes
+    
+    let mystr = null;
+    if (minutesisnotvalid);
+    else
+    {
+        let roundnearestfive = (minutes < 30);
+        const myroundnum = (roundnearestfive ? 5 : 10);
+        let numcupsorbkfst = Math.trunc((minutes / myroundnum) + ((minutes % myroundnum === 0) ? 0 : 1));
+        mystr = "•";
+        for (let i = 0; i < numcupsorbkfst; i++)
+        {
+            if (roundnearestfive) mystr += mycupimgchar;
+            else mystr += mybrkfstimgchar;
+        }
+        mystr += "" + minutes + " min read";
+    }
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} {mystr}</small>
             <p>{preview}</p>
         </article>
     );
